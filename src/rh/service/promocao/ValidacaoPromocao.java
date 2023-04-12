1702da9eac279ec6a723eccde026fda9aac54aab
Marco Tulio <marcotfrancosilva@gmail.com>
2023-04-12 17:37:29 -0300

    depency inversion principle identificado

--- src/rh/service/promocao/ValidacaoPromocao.java
+++ src/rh/service/promocao/ValidacaoPromocao.java
@@ -2,6 +2,9 @@ package rh.service.promocao;
 
 import rh.model.Funcionario;
 
+/**
+ * Dependency Inversion Principle implementado
+ */
 public interface ValidacaoPromocao {
     void valida(Funcionario funcionario, boolean meta);
 
--- src/rh/service/reajuste/ValidacaoReajuste.java
+++ src/rh/service/reajuste/ValidacaoReajuste.java
@@ -4,6 +4,9 @@ import rh.model.Funcionario;
 
 import java.math.BigDecimal;
 
+/**
+ * Dependency Inversion Principle implementado
+ */
 public interface ValidacaoReajuste {
 
     public void valida(Funcionario funcionario, BigDecimal aumento);
